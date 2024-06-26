aa70c918341e16b67f68875e6959eb160ec56933
sanketpatil1699 <83230890+sanketpatil1699@users.noreply.github.com>
2024-06-26 22:33:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './App.css'; // Import the CSS file
+import './App.css';
 
 const App = () => {
     const [employees, setEmployees] = useState([]);
